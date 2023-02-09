26eed6005f5b478bed980f6cee6e2c490af51d46
phquang98 <53476449+phquang98@users.noreply.github.com>
2023-02-09 14:30:54 +0700

    feat: ini commit

---
+++ testDelAPI/Models/Pokemon.cs
@@ -0,0 +1,13 @@
+﻿namespace testDelAPI.Models
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime DOB { get; set; }
+        public ICollection<Review>? ReviewClt { get; set; }
+        public ICollection<PokemonOwner> PokemonOwnerClt { get; set; }
+        public ICollection<PokemonCategory> PokemonCategoryClt { get; set; }
+
+    }
+}
---
+++ testDelAPI/Models/Reviewer.cs
@@ -0,0 +1,10 @@
+﻿namespace testDelAPI.Models
+{
+    public class Reviewer
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public ICollection<Review>? ReviewClt { get; set;}
+    }
+}
